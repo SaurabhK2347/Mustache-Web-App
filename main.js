5d081ee199a179ea6c157347609952df85648164
SaurabhK2347 <81204284+SaurabhK2347@users.noreply.github.com>
2022-02-19 15:43:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload() 
 {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 
 }
 
@@ -23,6 +27,7 @@ function modelLoaded()
 function draw() 
 {
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 30, 30);
 
 }
 
@@ -36,6 +41,8 @@ function gotPoses(results)
     if(results.length > 0) 
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
